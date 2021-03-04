5cd89b34ae8ad37ba53c96c0c09b449165266003
Maciej Bielecki <zyla@prati.pl>
2021-03-04 19:42:26 +0100

    Make myvariant actually efficient [ci skip]

--- random/myvariant.h
+++ random/myvariant.h
@@ -25,7 +25,21 @@ struct myunion<T, Ts...> {
 
 template <>
 struct myunion<> {};
- 
+
+template<typename R, typename T, typename F>
+R visit_union(int, const myunion<T> &u, F f) {
+    return f(u.value.here);
+}
+
+template<typename R, typename T, typename ...Ts, typename F, typename ...Fs>
+R visit_union(int index, const myunion<T, Ts...> &u, F f, Fs... fs) {
+    if(index == 0) {
+        return f(u.value.here);
+    } else {
+        return visit_union<R>(index - 1, u.value.there, fs...);
+    }
+}
+
 /**
  * A tagged union, i.e. an union which stores which union member is active.
  * Each type parameter is a possible union member.
@@ -35,22 +49,8 @@ struct myvariant {
     int v;
     myunion<Ts...> u;
 
-    template<typename R>
-    R visit(std::function<R(Ts)>... fs) const {
-        return visit_union(this->v, this->u, fs...);
+    template<typename R, typename ...Fs>
+    R visit(Fs... fs) const {
+        return visit_union<R>(this->v, this->u, fs...);
     }
 };
-
-template<typename R, typename T>
-R visit_union(int, const myunion<T> &u, std::function<R(T)> f) {
-    return f(u.value.here);
-}
-
-template<typename R, typename T, typename ...Ts>
-R visit_union(int index, const myunion<T, Ts...> &u, std::function<R(T)> f, std::function<R(Ts)>... fs) {
-    if(index == 0) {
-        return f(u.value.here);
-    } else {
-        return visit_union(index - 1, u.value.there, fs...);
-    }
-}
