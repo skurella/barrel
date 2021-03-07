8819ad26aac8f525c780407b54451f8845952a04
Maciej Bielecki <zyla@prati.pl>
2021-03-07 15:57:33 +0100

    random variant_test chane

--- random/variant_test.cpp
+++ random/variant_test.cpp
@@ -3,7 +3,9 @@
 #include <variant>
 #include <functional>
 
-#include "myvariant.h"
+#include "../utils/variant.h"
+
+using utils::variant;
  
 struct Stop {
 };
@@ -11,31 +13,14 @@ struct Data {
     int payload;
 };
 
-using packet_t = std::variant<Stop, Data>;
+using packet_t = variant<Stop, Data>;
 
 int main() {
-    std::cout
-      << std::boolalpha << "Stop is trivially copy-constructible? "
-      << std::is_trivially_copy_constructible<Stop>::value
-      << std::endl;
-
-    std::cout
-      << std::boolalpha << "Data is trivially copy-constructible? "
-      << std::is_trivially_copy_constructible<Data>::value
-      << std::endl;
-
-    std::cout
-      << std::boolalpha << "variant<int, bool> is trivially copy-constructible? "
-      << std::is_trivially_copy_constructible<std::variant<int, bool>>::value
-      << std::endl;
-
     std::cout
       << std::boolalpha << "packet_t is trivially copy-constructible? "
       << std::is_trivially_copy_constructible<packet_t>::value
       << std::endl;
 
-    std::cout
-      << std::boolalpha << "myvariant is trivially copy-constructible? "
-      << std::is_trivially_copy_constructible<myvariant<Stop, Data>>::value
-      << std::endl;
+//    packet_t p{Stop{}};
+//    packet_t p1;
 }
